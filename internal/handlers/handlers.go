91dbc38e655b2a56ec5160aa5e55e52e14656a38
sergei lavrushin <serglavr92gmail.com>
2025-12-09 03:39:10 +0300

    abobaaaaaaa

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,7 +16,6 @@ import (
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html")
 	filePath := "../index.html"
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
@@ -29,7 +28,7 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	if r.Method != http.MethodPost {
 		log.Printf("%v method not allowed", http.StatusMethodNotAllowed)
