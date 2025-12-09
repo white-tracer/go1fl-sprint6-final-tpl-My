72615571a3a7b04d9c5051430140c9e762fb5a95
sergei lavrushin <serglavr92gmail.com>
2025-12-09 03:24:46 +0300

    another-test

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,6 +16,7 @@ import (
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	filePath := "../index.html"
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
